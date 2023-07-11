be9ef24f2b7e9240557ffc6538478b3e0736896a
Justinecirera <128766246+Justinecirera@users.noreply.github.com>
2023-07-11 18:25:11 +0800

    Logo x Navbar
    Added Logo and Fixes Nav Bar

--- resources/views/layout/usernav.blade.php
+++ resources/views/layout/usernav.blade.php
@@ -6,7 +6,9 @@
       <div class="container-fluid">
         <div class="row">
           <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top col-12 col-lg-2">
-            <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+            <a class="navbar-brand" href="{{ route('dashboard') }}">
+            <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" class="logo-image">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -14,60 +16,60 @@
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav flex-column">
                 <li class="nav-item">
-                  <a class="nav-link" href="{{ route('dashboard') }}"> <img class="images1" src="{{ asset ('assets/img/dashboard.png') }}" style="width: 1vw;
-                    height: 100%;
-                    object-fit: cover;"> Dashboard</a>
+                  <a class="nav-link" href="{{ route('dashboard') }}"> <img class="images1">Dashboard</a>
                 </li>
-                <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="dropdown01" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            <img class="images" src="{{ asset ('assets/img/courses.png') }}"> Resources
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="dropdown01">
-                            @foreach ($colleges as $college)
-                                <li class="dropdown dropdown-submenu">
-                                    <a class="dropdown-item dropdown-toggle" id="dropdown{{ $college->id }}"
-                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        {{ $college->collegeName }}
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdown{{ $college->id }}">
-                                        @foreach ($college->courses as $course)
-                                            <li><a class="dropdown-item" href="{{ route('show.subjects', ['course_id' => $course->id]) }}">{{ $course->courseName }}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
+                <li class="nav-item dropend">
+                  <a class="nav-link dropdown-toggle" id="dropdown01" data-bs-toggle="dropdown" aria-haspopup="true"
+                  aria-expanded="false">
+                  <img class="images">Resources
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown01">
+                  @foreach ($colleges as $college)
+                  <li class="dropdown dropdown-submenu">
+                    <a class="dropdown-item dropdown-toggle" id="dropdown{{ $college->id }}" data-bs-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    {{ $college->collegeName }}
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="dropdown{{ $college->id }}">
+                    @foreach ($college->courses as $course)
+                    <li><a class="dropdown-item"
+                    href="{{ route('show.subjects', ['course_id' => $course->id]) }}">{{ $course->courseName }}</a>
+                  </li>
+                  @endforeach
+                </ul>
+              </li>
+              @endforeach
+            </ul>
+          </li>
                 
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" id="dropdown2"
-                  data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="imgages" src="{{ asset ('assets/img/personal.png') }}"> Personal</a>
-                  <div class="dropdown-menu" aria-labelledby="dropdown2">
-                    <a class="dropdown-item" href="#">Notes History</a>
-                    <a class="dropdown-item" href="/journals">Study Journal</a>
-                    <a class="dropdown-item" href="/favorites">Favorites</a>
-                  </div>
+          <li class="nav-item dropend">
+            <a class="nav-link dropdown-toggle" id="dropdown2" data-bs-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <img>Personal
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown2">
+            <a class="dropdown-item" href="#">Notes History</a>
+            <a class="dropdown-item" href="/journals">Study Journal</a>
+            <a class="dropdown-item" href="/favorites">Favorites</a>
+          </ul>
+        </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="{{ route ('discussions.index') }}"><img class="images1" src="{{ asset ('assets/img/forum.png') }}"> Forum</a>
+                  <a class="nav-link" href="{{ route ('discussions.index') }}"><img class="images1">Forum</a>
                 </li>
                 
                 <!-- Button available for the following role only -->
                 @if (auth()->user()->role_id === 2)
                 <li class="nav-item">
-                  <a class="nav-link" href="{{ url ('teachermanage') }}"><img class="images1" src="{{ asset ('assets/img/forum.png') }}"> Resources</a>
+                  <a class="nav-link" href="{{ url ('teachermanage') }}"><img class="images1">Resources</a>
                 </li>
                 @endif
 
                 <li class="nav-item">
-                  <a class="nav-link" href="{{route('settings')}}"><img class="images1" src="{{ asset ('assets/img/setting.png') }}"> Settings</a>
+                  <a class="nav-link" href="{{route('settings')}}"><img class="images1">Settings</a>
                 </li>
 
                 <li class="nav-item">
-                  <a class="nav-link" href="#" onclick="confirmLogout()" style="cursor: pointer;"><img class="images1" src="{{ asset ('assets/img/logout.png') }}" style="width: 1vw;
-                    height: 100%;
-                    object-fit: cover;"> Logout</a>
+                  <a class="nav-link" href="#" onclick="confirmLogout()"><img class="images1">Logout</a>
                 </li>
               </ul>
             </div>
