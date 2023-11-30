15908553f049cb9ec9d33e9be4802e0f35b9e86b
Justinecirera <128766246+Justinecirera@users.noreply.github.com>
2023-12-01 01:33:44 +0800

    Revert "Backup Test Production - 3"
    This reverts commit 1c784edf27c59a7acdf9a035e4d971b3e21d4018.

--- app/Http/Controllers/BackupRestoreController.php
+++ app/Http/Controllers/BackupRestoreController.php
@@ -50,7 +50,7 @@ class BackupRestoreController extends Controller
     {
         try {
             // Run the backup command using Artisan
-            \Artisan::call('backup:run', ['--only-db' => true, '--destination' => 'hostinger']);
+            \Artisan::call('backup:run');
 
             // Check if the backup command was successful
             if (\Artisan::output() === 'Backup completed successfully.') {
--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
         
         'hostinger' => [
             'driver' => 'ftp',
-            'host' => 'gener-lnulib.site',
+            'host' => 'ftp://gener-lnulib.site',
             'username' => 'u203878552',
             'password' => 'Generlnu123!',
             'port' => 21,
